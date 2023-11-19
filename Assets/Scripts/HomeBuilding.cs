8e78cc2a1306c1ba957b4be93a66cc88e54adc9b
Mert Çakar <mertcakar.es@gmail.com>
2023-11-19 09:30:36 +0300

    Sky color changed on time change.

--- Assets/Scripts/HomeBuilding.cs
+++ Assets/Scripts/HomeBuilding.cs
@@ -6,6 +6,7 @@ public class HomeBuilding : MonoBehaviour
     [SerializeField] private TMP_Text dayInfoText;
     [SerializeField] private TMP_Text hourInfoText;
     [SerializeField] private GameObject paperObject;
+    [SerializeField] private Camera camera;
 
     private int dayCount = 1;
 
@@ -20,6 +21,7 @@ public class HomeBuilding : MonoBehaviour
 
         dayInfoText.text = ++dayCount + ". Gün";
         hourInfoText.text = "09:00";
+        camera.backgroundColor = new Color(79f / 255f, 114f / 255f, 168f / 255f, 0);
         if (dayCount == 3)
         {
             paperObject.SetActive(true);
--- Assets/Scripts/WorkBuilding.cs
+++ Assets/Scripts/WorkBuilding.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 public class WorkBuilding : MonoBehaviour
 {
     [SerializeField] private TMP_Text hourInfoText;
+    [SerializeField] private Camera camera;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         hourInfoText.text = "18:00";
+        camera.backgroundColor = new Color(33f / 255f, 45f / 255f, 65f / 255f, 0);
     }
 
 }
